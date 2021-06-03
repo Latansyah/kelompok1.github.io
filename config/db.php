ddd4810a52ee9d28f8c8270a85fd8fca73f592ff
LATANSYAH MAULANA VIRNANDANI <81965349+Latansyah@users.noreply.github.com>
2021-06-03 09:38:18 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,27 @@
 <?php
 
-try {
-	$conn = new mysqli('localhost', 'root', '','alat');
-} catch (Exception $e) {
-	echo $e->getMessage();
-}
+//Remote Database Connection
+   $host = 'remotemysql.com';
+   $db = '46LiVaQ1Zg';
+   $user = '46LiVaQ1Zg';
+   $pass = 'Njv8VPOJVv';
+  $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try{
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch(PDOException $e) {
+        throw new PDOException($e->getMessage());
+    }
+
+    require_once 'crud.php';
+    require_once 'user.php';
+    $crud = new crud($pdo);
+    $user = new user($pdo);
+   
+    $user->insertUser("admin","password");
+?>
+© 2021 GitHub, Inc.
